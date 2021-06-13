87392957ee38ca32a0cb3863701dea664b169986
Nikolina5 <79012123+Nikolina5@users.noreply.github.com>
2021-06-14 00:43:03 +0200

    Update minor-processor.js

--- Lecture1/minor-processor.js
+++ Lecture1/minor-processor.js
@@ -1,22 +1,11 @@
-  module.exports = people => {
-    let counter = 0;
-  
-    
-    people.forEach(person => {
-      if (person.age < 18) {
-        counter++;
-        console.log(`Ime ${person.name}, prezime: ${person.lastname}`);
-      } else if (person.age > 18 && person.age < 21) {
-        counter++;
-        console.log(`Ime ${person.name}, prezime: ${person.lastname}`);
-      } else if (person.age > 21 && person.age < 25) {
-        counter++;
-        console.log(`Ime ${person.name}, prezime: ${person.lastname}`);
-      } else{
-        console.log("For now, we havent't got minors in the organization.");
-      }   
+module.exports = (people,age) => {
+    let counter=0;
+
+    people.forEach( person=> {
+        if(person.age<age) {
+            counter++;
+            console.log(`Name: ${person.name}, Last Name: ${person.lastname}`);
+        }
     });
-        console.log("We have " + counter + " minors in our organization.");
-
-  };
-
+    console.log("${counter} persons.");
+};
