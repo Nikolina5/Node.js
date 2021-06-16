a379b683a4a516efdb144002593779023e5cc723
Nikolina Eliskovska <eliskovskan@hotmail.com>
2021-06-16 22:37:14 +0200

    Second homework

---
+++ Lecture1/minor-processor.js
@@ -0,0 +1,11 @@
+module.exports = (people,age) => {
+  let counter=0;
+
+  people.forEach( person=> {
+      if(person.age<age) {
+          counter++;
+          console.log(`Name: ${person.name}, Last Name: ${person.lastname}`);
+      }
+  });
+  console.log("${counter} persons.");
+};
